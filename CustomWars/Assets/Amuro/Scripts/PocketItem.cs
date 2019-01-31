e3d16767ed97376b79afe08261c9cc9e2e8ac1c4
DaiyaKawasaki_c17006 <c17006@std.it-college.ac.jp>
2019-01-31 16:48:07 +0900

    UnityDataを追加

---
+++ CustomWars/Assets/Amuro/Scripts/PocketItem.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PocketItem : MonoBehaviour
+{
+    private ItemData myItemData;
+
+    private MyItemStatus myItemStatus;
+
+    // private ProcessingSlot processingSlot;
+
+    private DragSlot dragSlot;
+
+    [SerializeField]
+    private PocketStatus pocketStatus;
+
+    private GameObject instanceDragItemUI;
+
+    private GameObject itemSlot;
+
+    [SerializeField]
+    private GameObject dragItemUI;
+
+    private GameObject customSlot;
+
+    private Transform customslot_c;
+
+    private GameObject batsu;
+
+    public static GameObject thisPocket;
+
+    [SerializeField]
+    private int slotNum;
+
+    private int itemNum;
+
+    private bool panelParam;                                //パネルにアイテムが入っているかどうか
+
+    [SerializeField]
+    private Text informationText;
+
+    private void Start()
+    {
+        pocketStatus = FindObjectOfType<PocketStatus>();
+        customSlot = GameObject.Find("custom_panel");
+        customslot_c = customSlot.GetComponentInChildren<Transform>();
+        batsu = this.transform.GetChild(1).gameObject;
+        // myItemStatus = FindObjectOfType<MyItemStatus>();
+    }
+
+    public void InitMyItemStatus(PlayerController myPlayer)
+    {
+        this.myItemStatus = myPlayer.GetComponent<PlayerController>().GetMyItemStatus();
+    }
+
+    public GameObject GetPocketData()
+    {
+        return thisPocket;
+    }
+    public void MouseDrop()                                 //　スロットの上にアイテムがドロップされた時に実行
+    {
+        if (FindObjectOfType<DragSlot>() == null || panelParam)
+        {
+            return;
+        }
+
+        foreach (Transform child in customslot_c)
+            {
+                child.gameObject.transform.GetComponent<CustomSlot>().BatsuEnd();
+            }
+
+        AudioManager.Instance.PlaySE("closing-wooden-door-1");
+        transform.GetChild(0).GetComponent<Image>().sprite = null;
+        myItemData = null;
+
+        dragSlot = FindObjectOfType<DragSlot>();       //　DragItemUIに設定しているDragItemDataスクリプトからアイテムデータを取得
+        myItemData = dragSlot.GetItem();
+        int id = myItemData.GetItemId();
+
+        if (id > 2 && panelParam == false)
+        {
+            itemSlot = dragSlot.GetSlotData();            //ドラッグしてきた持ち物パネルを取得持ち物
+            //Debug.Log(itemSlot);
+
+            ShowInformation();
+
+            //カスタムパネルに装備
+            panelParam = true;
+            pocketStatus.SetItemData(myItemData, slotNum);
+
+
+            switch (dragSlot.GetDeleteNum())
+            {
+                case 1:
+                    myItemStatus.SetItemCount(id, -1);
+                    ProcessingSlot processingSlot = itemSlot.GetComponent<ProcessingSlot>();
+                    StartCoroutine(processingSlot.displayCount());
+
+                    if (myItemStatus.GetItemCount(id) <= 0)
+                    {
+                        myItemStatus.SetItemFlag(id, false);
+                        itemSlot.GetComponent<ProcessingSlot>().PanelDelete();
+                    }
+
+                    break;
+
+                case 3:
+
+                    itemSlot.GetComponent<PocketItem>().PanelDelete();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        dragSlot.DeleteDragItem();                          //　ドラッグしているアイテムデータの削除
+        //pocketStatus.SetItemData(myItemData, slotNum);
+    }
+
+
+    void ShowInformation()
+    {
+        transform.GetChild(0).GetComponent<Image>().sprite = myItemData.GetItemSprite();        //　アイテムイメージを設定
+
+    }
+
+
+    public void MouseBeginDrag()                                                                //パネルをドラッグした際にアイテム画像を生成
+    {
+        if (!panelParam)
+        {
+            return;
+        }
+        
+        foreach (Transform child in customslot_c)
+            {
+                child.gameObject.transform.GetComponent<CustomSlot>().BatsuPanel();
+            }
+
+        AudioManager.Instance.PlaySE("cancel2");
+        instanceDragItemUI = Instantiate(dragItemUI, Input.mousePosition, Quaternion.identity) as GameObject;
+        instanceDragItemUI.transform.SetParent(transform.parent.parent);
+        instanceDragItemUI.GetComponent<DragSlot>().SetDragItem(myItemData, gameObject, 3);
+    }
+
+
+    public void MouseEndDrag()                                                                   //ドラッグ終了時にアイテム画像を削除
+    {
+        foreach (Transform child in customslot_c)
+            {
+                child.gameObject.transform.GetComponent<CustomSlot>().BatsuEnd();
+            }
+
+        Destroy(instanceDragItemUI);
+    }
+
+    public void BatsuPanel()
+    {
+        batsu.SetActive(true);
+    }
+
+    public void BatsuEnd()
+    {
+        batsu.SetActive(false);
+    }
+
+    public void PanelDelete()
+    {
+        transform.GetChild(0).GetComponent<Image>().sprite = null;
+        myItemData = null;
+        panelParam = false;
+        pocketStatus.SetItemDelete(slotNum);
+    }
+}
---
+++ CustomWars/Assets/Amuro/Scripts/ProcessingSlot.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProcessingSlot : MonoBehaviour
+{
+    [SerializeField]
+    public ItemData myItemData;             //自身のアイテムデータ
+
+    [SerializeField]
+    private MyItemStatus myItemStatus;
+
+    [SerializeField]
+    private GameObject titleUI;         //アイテム名を表示するテキストUIプレハブ
+
+    [SerializeField]
+    private GameObject dragItemUI;
+
+    private GameObject instanceDragItemUI;  //アイテムをドラッグしたときに生成する画像
+
+    private GameObject customSlot;
+
+    private Transform customslot_c;
+
+    private GameObject itemSlot;      //アイテムスロット格納用
+
+    private Transform itemslot_c;
+    
+    private Text informationText;       //アイテム情報を表示するテキストUI    
+
+    private Text nameText;
+
+    private Text countText;             //所持数を表示
+
+    private bool countdisplay;
+
+    private int id;
+
+
+    void OnDisable()                        //スロットが非アクティブになったら削除
+    {
+        Destroy(gameObject);
+    }
+
+
+    public void SetItemData(ItemData itemData)      //アイテムデータの取得と、アイテムイメージの表示
+    {
+        myItemData = itemData;
+        transform.GetChild(2).GetComponent<Image>().sprite = myItemData.GetItemSprite();
+    }
+
+
+    void Start()    //アイテム名とアイテム説明の取得、表示
+    {
+        nameText = transform.Find("Name").GetChild(0).GetComponent<Text>();
+        informationText = transform.Find("Information").GetChild(0).GetComponent<Text>();
+
+        customSlot = GameObject.Find("custom_panel");
+        itemSlot = GameObject.Find("pocket_panel");
+        customslot_c = customSlot.GetComponentInChildren<Transform>();
+        itemslot_c = itemSlot.GetComponentInChildren<Transform>();
+        id = myItemData.GetItemId();
+
+        nameText.text = myItemData.GetItemName();
+        informationText.text = myItemData.GetItemInformation();
+
+        if (id <= 2)
+        {
+            transform.GetChild(1).transform.GetComponent<Image>().color = new Color(255, 0, 0, 100);
+        }
+        else
+        {
+            transform.GetChild(1).transform.GetComponent<Image>().color = new Color(0, 0, 255, 100);
+        }
+
+        StartCoroutine(displayCount());
+    }
+
+    public IEnumerator displayCount()
+    {
+        myItemStatus = gameObject.transform.root.GetComponent<PlayerUIController>().GetMyItemStatus();
+        countText = transform.Find("Count").GetChild(0).GetComponent<Text>();
+
+        if (myItemStatus.GetItemCount(myItemData.GetItemId()) > 1)
+        {
+            transform.Find("Count").gameObject.SetActive(true);
+            countText.text = "×" + myItemStatus.GetItemCount(myItemData.GetItemId()).ToString();
+        }
+        else if (myItemStatus.GetItemCount(myItemData.GetItemId()) <= 1)
+        {
+            transform.Find("Count").gameObject.SetActive(false);
+        }
+
+        yield break;
+    }
+
+
+    public void MouseBeginDrag()        //アイテムをドラッグしたときの挙動
+    {
+        instanceDragItemUI = Instantiate(dragItemUI, Input.mousePosition, Quaternion.identity) as GameObject;
+        instanceDragItemUI.transform.SetParent(transform.parent.parent.parent.parent);
+        instanceDragItemUI.GetComponent<DragSlot>().SetDragItem(myItemData, gameObject, 1);
+
+        if (id <= 2)
+        {
+            foreach (Transform child in itemslot_c)
+            {
+                child.gameObject.transform.GetComponent<PocketItem>().BatsuPanel();           
+            }
+        }
+        else
+        {
+            foreach (Transform child in customslot_c)
+            {
+                child.gameObject.transform.GetComponent<CustomSlot>().BatsuPanel();
+            }
+        }
+    }
+
+
+    public void MouseEndDrag()          //ドラッグ終了時にアイテム画像を削除
+    {
+        foreach (Transform child in itemslot_c)
+            {
+                child.gameObject.transform.GetComponent<PocketItem>().BatsuEnd();           
+            }
+
+        foreach (Transform child in customslot_c)
+            {
+                child.gameObject.transform.GetComponent<CustomSlot>().BatsuEnd();
+            }
+
+        Destroy(instanceDragItemUI);
+    }
+
+    public void PanelDelete()
+    {
+        Destroy(gameObject);
+    }
+
+}
